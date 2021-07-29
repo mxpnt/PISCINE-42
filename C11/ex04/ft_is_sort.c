9878ba55a4c7ea505a91dd7f85f61bf7d04d94dc
mxpnt <87723293+mxpnt@users.noreply.github.com>
2021-07-29 10:53:08 +0200

    Add files via upload

---
+++ C11/ex04/ft_is_sort.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapontil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 08:06:33 by mapontil          #+#    #+#             */
+/*   Updated: 2021/07/29 10:30:58 by mapontil         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+	int	i;
+	int	countpos;
+	int	countneg;
+
+	i = 0;
+	countpos = 0;
+	countneg = 0;
+	if (length == 0)
+		return (1);
+	while (i < length - 1)
+	{
+		if ((*f)(tab[i], tab[i + 1]) >= 0)
+			countpos++;
+		i++;
+	}
+	i = 0;
+	while (i < length - 1)
+	{
+		if ((*f)(tab[i], tab[i + 1]) <= 0)
+			countneg++;
+		i++;
+	}
+	if (countpos == length - 1 || countneg == length - 1)
+		return (1);
+	return (0);
+}
